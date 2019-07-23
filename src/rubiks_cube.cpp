876d524cf4be27e9038fedd4390aab579e89c8e4
peakergzf <peakergzf@outlook.com>
2019-07-23 18:53:04 +1000

    refactor layer struct and add all clockwise roatations

--- src/rubiks_cube.cpp
+++ src/rubiks_cube.cpp
@@ -9,14 +9,20 @@ typedef vector<Color> vc;
 const int DIM = 3;
 const int N_FACES = 6;
 
-// pieces in a (non-middle) layer
+// indexes of the pieces in a (non-middle) layer
 struct Layer {
     //    ur um ul
     // ll tl tm tr rr
     // lm ml mm mr rm
     // lr bl bm br rl
     //    dl dm dr
-
+    
+    // constructor
+    Layer(int tl, int tm, int tr, int ml, int mm, int mr, int bl, int bm, int br,
+          int ll, int lm, int lr, int ul, int um, int ur, int rl, int rm, int rr, int dl, int dm, int dr): 
+        tl{tl}, tm{tm}, tr{tr}, ml{ml}, mm{mm}, mr{mr}, bl{bl}, bm{bm}, br{br},
+        ll{ll}, lm{lm}, lr{lr}, ul{ul}, um{um}, ur{ur}, rl{rl}, rm{rm}, rr{rr}, dl{dl}, dm{dm}, dr{dr}  {}
+    
     // current (front) face
     int tl, tm, tr; // topLeft, topMid, topRight
     int ml, mm, mr; // midLeft, midMid, midRight
@@ -50,6 +56,50 @@ struct RubiksCube {
         RD, RD, RD, RD, RD, RD, RD, RD, RD  // right
     };
 
+    // ------------------------------------------------------------------------
+
+    //    ur um ul    |    33 34 35 
+    // ll tl tm tr rr | 36  0  1  2 47
+    // lm ml mm mr rm | 37  3  4  5 46
+    // lr bl bm br rl | 38  6  7  8 45
+    //    dl dm dr    |    18 19 20 
+    Layer up {0, 1, 2, 3, 4, 5, 6, 7, 8, 36, 37, 38, 35, 34, 33, 45, 46, 47, 18, 19, 20};
+
+    //    ur um ul    |    24 25 26 
+    // ll tl tm tr rr | 44  9 10 11 51
+    // lm ml mm mr rm | 43 12 13 14 52
+    // lr bl bm br rl | 42 15 16 17 53
+    //    dl dm dr    |    27 28 29 
+    Layer down {9, 10, 11, 12, 13, 14, 15, 16, 17, 44, 43, 42, 26, 25, 24, 53, 52, 51, 27, 28, 29};
+
+    //    ur um ul    |     6  7  8 
+    // ll tl tm tr rr | 38 18 19 20 45
+    // lm ml mm mr rm | 41 21 22 23 48
+    // lr bl bm br rl | 44 24 25 26 51
+    //    dl dm dr    |     9 10 11 
+    Layer front {18, 19, 20, 21, 22, 23, 24, 25, 26, 38, 41, 44, 8, 7, 6, 51, 48, 45, 9, 10, 11};
+
+    //    ur um ul    |    15 16 17 
+    // ll tl tm tr rr | 42 27 28 29 53
+    // lm ml mm mr rm | 39 30 31 32 50
+    // lr bl bm br rl | 36 33 34 35 47
+    //    dl dm dr    |     0  1  2 
+    Layer back {27, 28, 29, 30, 31, 32, 33, 34, 35, 42, 39, 36, 17, 16, 15, 47, 50, 53, 0, 1, 2};
+
+    //    ur um ul    |     0  3  6 
+    // ll tl tm tr rr | 33 36 37 38 18
+    // lm ml mm mr rm | 30 39 40 41 21
+    // lr bl bm br rl | 27 42 43 44 24
+    //    dl dm dr    |    15 12  9 
+    Layer left {36, 37, 38, 39, 40, 41, 42, 43, 44, 33, 30, 27, 6, 3, 0, 24, 21, 18, 15, 12, 9};
+
+    //    ur um ul    |     8  5  2 
+    // ll tl tm tr rr | 20 45 46 47 35
+    // lm ml mm mr rm | 23 48 49 50 32
+    // lr bl bm br rl | 25 51 52 53 29
+    //    dl dm dr    |    11 14 17 
+    Layer right {45, 46, 47, 48, 49, 50, 51, 52, 53, 20, 23, 25, 2, 5, 8, 29, 32, 35, 11, 14, 17};
+
     // ========================================================================
     // face rotations
     // ========================================================================
@@ -78,82 +128,73 @@ struct RubiksCube {
         cube[x.rl] = prev[x.ul], cube[x.rm] = prev[x.um], cube[x.rr] = prev[x.ur];
         // down face
         cube[x.dl] = prev[x.rl], cube[x.dm] = prev[x.rm], cube[x.dr] = prev[x.rr];
-
     }
 
     // up face clockwise
-    void U() {
-        Layer x;
-        //    ur um ul    |    33 34 35 
-        // ll tl tm tr rr | 36  0  1  2 47
-        // lm ml mm mr rm | 37  3  4  5 46
-        // lr bl bm br rl | 38  6  7  8 45
-        //    dl dm dr    |    18 19 20 
-        x.tl = 0, x.tm = 1, x.tr = 2;
-        x.ml = 3, x.mm = 4, x.rr = 5;
-        x.bl = 6, x.bm = 7, x.br = 8;
-        x.ll = 36, x.lm = 37, x.lr = 38;
-        x.ul = 35, x.um = 34, x.ur = 33;
-        x.rl = 45, x.rm = 46, x.rr = 47;
-        x.dl = 18, x.dm = 19, x.dr = 20;
-        clockwiseRotate(x);
-    }
+    void U() {clockwiseRotate(up);}
 
     // down face clockwise
-    void D() {
+    void D() {clockwiseRotate(down);}
 
-    }
     // front face clockwise
-    void F() {
-
-    }
+    void F() {clockwiseRotate(front);}
 
     // back face clockwise
-    void B() {
-
-    }
+    void B() {clockwiseRotate(back);}
     
     // left face clockwise
-    void L() {
-
-    }
+    void L() {clockwiseRotate(left);}
     
     // right face clockwise
-    void R() {
-
-    }
+    void R() {clockwiseRotate(right);}
     
     // ------------------------------------------------------------------------
+    
+    // TODO
+    void counterClockwiseRotate(Layer x) {
+        // //    ur um ul    |    lr lm ll
+        // // ll tl tm tr rr | dl bl ml tl ur
+        // // lm ml mm mr rm | dm bm mm tm um
+        // // lr bl bm br rl | dr br mr tr ul
+        // //    dl dm dr    |    rl rm rr
+        
+        // vc prev(cube);
+        
+        // // current (front) face
+        // cube[x.tl] = prev[x.bl], cube[x.tm] = prev[x.ml], cube[x.tr] = prev[x.tl];
+        // cube[x.ml] = prev[x.bm], cube[x.mm] = prev[x.mm], cube[x.mr] = prev[x.tm];
+        // cube[x.bl] = prev[x.br], cube[x.bm] = prev[x.mr], cube[x.br] = prev[x.tr];
+        
+        // // left face
+        // cube[x.ll] = prev[x.dl], cube[x.lm] = prev[x.dm], cube[x.lr] = prev[x.dr];
+        
+        // // up face
+        // cube[x.ul] = prev[x.ll], cube[x.um] = prev[x.lm], cube[x.ur] = prev[x.lr];
+
+        // // right face
+        // cube[x.rl] = prev[x.ul], cube[x.rm] = prev[x.um], cube[x.rr] = prev[x.ur];
+        // // down face
+        // cube[x.dl] = prev[x.rl], cube[x.dm] = prev[x.rm], cube[x.dr] = prev[x.rr];
+    }
 
     // up face counterclockwise
-    void U_() {
-
-    }
+    void U_() {counterClockwiseRotate(up);}
 
     // down face counterclockwise
-    void D_() {
-
-    }
+    void D_() {counterClockwiseRotate(down);}
 
     // front face counterclockwise
-    void F_() {
-
-    }
+    void F_() {counterClockwiseRotate(front);}
 
     // back face counterclockwise
-    void B_() {
-
-    }
+    void B_() {counterClockwiseRotate(back);}
     
     // left face counterclockwise
-    void L_() {
-
-    }
+    void L_() {counterClockwiseRotate(left);}
     
     // right face counterclockwise
-    void R_() {
+    void R_() {counterClockwiseRotate(right);}
 
-    }
 
     // ========================================================================
     // slice turns
@@ -228,6 +269,10 @@ struct RubiksCube {
 int main() {
     RubiksCube cube;
     cube.prettyPrint();
+    cube.B();
+    cube.L();
     cube.U();
+    cube.L();
+    cube.F();
     cube.prettyPrint();
 }
